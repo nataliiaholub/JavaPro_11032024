fbf86ffa92d1cccff9b8f883b99134eb2beb4978
Nataliia Holub <golub.togo@gmail.com>
2024-03-24 16:20:20 +0100

    Added interface GeometricFigure. Added classes Circle, Triangle, and Square which implement GeometricFigure interface. Implemented Main class for calculating total area of Circle, Triangle, and Square geometric shapes.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+
 public class Main {
 
+    public static void main(String[] args){
+
+        ArrayList<GeometricFigure> geometricFigures = new ArrayList<>();
+
+        geometricFigures.add(new Circle(2.0));
+        geometricFigures.add(new Triangle(5.0, 9.0));
+        geometricFigures.add(new Square(4.0));
+
+        double totalArea = calculateTotalArea(geometricFigures);
+
+        System.out.println("Total area of all shapes: " + totalArea);
+
+    }
+
+    private static double calculateTotalArea(ArrayList<GeometricFigure> shapes){
+        double totalArea = 0.0;
+        for (int i = 0; i < shapes.size(); i++) {
+            GeometricFigure shape = shapes.get(i);
+            totalArea +=shape.getArea();
+        }
+        return totalArea;
+    }
 }
