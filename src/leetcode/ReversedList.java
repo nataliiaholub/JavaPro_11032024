71e47d48e1d183cffdd504250a0726d5b53693e1
Nataliia Holub <golub.togo@gmail.com>
2024-04-07 12:13:39 +0200

    Added the implementation of the reverseList method to reverse a singly linked list.

---
+++ src/leetcode/ReversedList.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
