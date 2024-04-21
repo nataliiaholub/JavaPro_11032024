35daae60ed8be3134230299ea2bcb4cf4aae092b
nataliyagolub <golub.togo@gmail.com>
2024-04-21 13:29:00 +0200

    Implement two methods with O(n^2) and O(1) complexity to solve the Two Sum task.

--- src/hw9_Tasks/TwoSum.java
+++ src/hw9_Tasks/TwoSum.java
@@ -1,34 +1,35 @@
 package hw9_Tasks;
+import java.util.HashMap;
 
-    public class TwoSum {
+public class TwoSum {
+        //O(n^2)
         public static int[] twoSum(int[] nums, int target) {
             int[] result = new int[2];
-
             for (int i = 0; i < nums.length; i++) {
                 for (int j =i + 1; j < nums.length; j++){
-                    //todo
+                    if (nums[i] + nums[j] == target){
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
                 }
             }
             return result;
         }
+
+        //O(1)
+        public static int[] twoSum2(int[] nums, int target) {
+            int[] result = new int[2];
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int complement = target - nums[i];
+                    if (map.containsKey(target - nums[i])) {
+                        result[0] = map.get(target - nums[i]);
+                        result[1] = i;
+                }
+                    map.put(nums[i], i);
+            }
+            return result;
+        }
     }
-
-//todo
-
-//    public static void main(String[] args){
-//        int[] nums1 = {2, 7, 11, 15};
-//        int target1 = 9;
-//        int[] result1 = twoSum(nums1, target1);
-//        System.out.println("Output: [" + result1[0] + ", " + result1[1] + "]");
-//
-//        int[] nums2 = {3, 2, 4};
-//        int target2 = 6;
-//        int[] result2 = twoSum(nums2, target2);
-//        System.out.println("Output: [" + result2[0] + ", " + result2[1] + "]");
-//
-//        int[] nums3 = {3, 3};
-//        int target3 = 6;
-//        int[] result3 = twoSum(nums3, target3);
-//        System.out.println("Output: [" + result3[0] + ", " + result3[1] + "]");
-//    }
 
