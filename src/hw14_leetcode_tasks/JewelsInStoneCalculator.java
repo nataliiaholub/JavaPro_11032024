721fbfedd1b577adb94b64c8843ece074f003c70
nataliyagolub <golub.togo@gmail.com>
2024-06-01 17:32:29 +0200

    add Jewels in Stone calculator

---
+++ src/hw14_leetcode_tasks/JewelsInStoneCalculator.java
@@ -0,0 +1,26 @@
+package hw14_leetcode_tasks;
+
+public class JewelsInStoneCalculator {
+    //On
+    public int numJewelsInStoneOneLoop(String jewels, String stones) {
+        int counter = 0;
+        for (int i = 0; i < stones.length(); i++) { // On
+            if (jewels.indexOf(stones.charAt(i)) != -1)
+                counter++;
+        }
+        return counter;
+    }
+
+    //On2
+    public int numJewelsInStoneTwoLoops(String jewels, String stones) {
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            for (int j = 0; j < jewels.length(); j++) { // On2
+                if (stones.charAt(i) == jewels.charAt(j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
