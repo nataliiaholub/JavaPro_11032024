527e05390181b519095da3576a650d2b2619526e
nataliyagolub <golub.togo@gmail.com>
2024-06-06 22:55:57 +0200

    extend Identical Pairs calculator, On solution

--- src/hw14_leetcode_tasks/IdenticalPairsCalculator.java
+++ src/hw14_leetcode_tasks/IdenticalPairsCalculator.java
@@ -1,8 +1,11 @@
 package hw14_leetcode_tasks;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class IdenticalPairsCalculator {
 
-    public int identicalPairs(int[] nums) {
+    public int identicalPairsTwoLoops(int[] nums) {
         int counter = 0;
         for(int i = 0; i < nums.length -1; i++){
             for(int j = i + 1; j < nums.length; j++ ){ //On2
@@ -13,4 +16,17 @@ public class IdenticalPairsCalculator {
         }
         return counter;
     }
-}
+
+    public int identicalPairsMap(int[] nums) {
+        int counter = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n : nums){ //On
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int n = entry.getValue();
+            counter += n * (n - 1) / 2;
+            }
+        return counter;
+        }
+    }
